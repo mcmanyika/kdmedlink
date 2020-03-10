6782397ce73699aae6eebf986d2a7462f50c1b2a
Partson Manyika <mcmanyika@gmail.com>
2020-03-10 00:11:34 -0500

    changed debug false

--- django_ajax/settings.py
+++ django_ajax/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'q$ecs+m%k9@y35ubgx(jqg32%52fyfd8dybgw+p$k(ais1+6_o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['159.89.83.174']
 
