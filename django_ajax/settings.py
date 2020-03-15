0e012a1d3a914ebb186055a4d630c101c4163736
Dispatch <kdmedilink@Dispatchs-iMac.lan>
2020-03-14 19:48:43 -0500

    redo site

--- django_ajax/settings.py
+++ django_ajax/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'q$ecs+m%k9@y35ubgx(jqg32%52fyfd8dybgw+p$k(ais1+6_o'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['134.122.26.177']
+ALLOWED_HOSTS = []
 
 # Application definition
 
