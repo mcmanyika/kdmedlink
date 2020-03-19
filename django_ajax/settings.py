de7ca5d645c84b55e0671fbcfab37b356d8fe1ad
Dispatch <kdmedilink@Dispatchs-iMac.lan>
2020-03-18 22:32:13 -0500

    update stlyesheet

--- django_ajax/settings.py
+++ django_ajax/settings.py
@@ -25,6 +25,7 @@ SECRET_KEY = 'q$ecs+m%k9@y35ubgx(jqg32%52fyfd8dybgw+p$k(ais1+6_o'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
+
 ALLOWED_HOSTS = ['167.172.131.14']
 
 # Application definition
